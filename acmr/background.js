78d4c7d806ea40e9fde02dedebad03dc244ce3b6
zhanghui33 <zhanghui33@baidu.com>
2019-04-22 20:44:28 +0800

    1.0.0 版本

--- acmr/background.js
+++ acmr/background.js
@@ -4,248 +4,168 @@ const meetingCheckInUrl = 'http://meeting.baidu.com/web/checkIn';
 const SUCCESS = 0,
     FAIL_REQUEST = 1,
     FAIL_TIMEOUT = 2,
-    FAIL_COOKIE = 3;
+    FAIL_LOGIN = 3;
 
-// check website cookies
-function checkCookies() {
-    return new Promise((resolve, reject) => {
-        chrome.cookies.getAll({
-            domain: 'meeting.baidu.com'
-        }, function (cookies) {
-            if (cookies.length > 2) {
-                // cookies check success: login status
-                resolve({
-                    cookieCheckStatus: true
-                });
-            } else {
-                // cookies check failed: logout status
-                reject({
-                    cookieCheckStatus: false,
-                    status: FAIL_COOKIE,
-                    message: 'cookie checked fail'
-                });
-            }
+const failNotifyId = 'fail_info_notify';
+let loginFailFromMe = false;
+// main check action
+let timer;
+let runtime = 0;
+let isStopTimer = false;
+
+
+
+async function fetchRoomList() {
+    let url = `${meetingListUrl}?t=${Date.now()}`;
+
+    try {
+        const res = await fetch(url, {
+            credentials: 'include'  
         });
-    });
-}
-
-// send request to check in
-function sendCheckInRequest(roomId) {
-    return new Promise((resolve, reject) => {
-        let url = `${meetingCheckInUrl}?scheduleId=${roomId}&random=${Math.random()}`;
-        let xhr = new XMLHttpRequest();
-        xhr.open('GET', url, true);
-        xhr.send();
-        xhr.onreadystatechange = function (e) {
-            try {
-                if (xhr.readyState === 4 && xhr.status === 200) {
-                    resolve({
-                        checkInStatus: SUCCESS
-                    });
-                } else if (xhr.readyState === 4 && xhr.status !== 200) {
-                    console.log(`Check fail ${url}`);
-                    throw Error(`Fail to checkin the room, request error: ${xhr.status}`);
-                }
-            } catch (err) {
-                reject({
-                    checkInStatus: FAIL_REQUEST,
-                    roomId,
-                    err,
-                });
-            }
-        };
-        xhr.onerror = function (e) {
-            console.log(e);
-            reject({
-                checkInStatus: FAIL_REQUEST,
-                roomId,
-                message: 'Fail to checkin the room, request error'
+        if (res.status !== 200) {
+            return await Promise.reject({
+                message: `Fail to get the room list! request error: ${res.status}`
             });
-        };
-        xhr.ontimeout = function () {
-            reject({
-                checkInStatus: FAIL_TIMEOUT,
-                roomId,
-                message: 'Fail to checkin the room, request timeout'
-            });
-        };
-    });
-}
-
-function checkRooms(res) {
-    const cookieCheckStatus = res.cookieCheckStatus;
-    return new Promise((resolve, reject) => {
-        if (cookieCheckStatus) {
-            let xhr = new XMLHttpRequest();
-            let url = `${meetingListUrl}?t=${Date.now()}`;
-            xhr.open('GET', url, true);
-            xhr.responseType = 'json';
-            xhr.send();
-            xhr.onreadystatechange = function (e) {
-                try {
-                    if (xhr.readyState === 4 && xhr.status === 200) {
-                        let json = xhr.response;
-                        let roomList = json.data.list.reduce(function (resultArr, item) {
-                            // status_field   havn't_start  can_check_in  hava_checked_in
-                            // canCancel      1             1             0
-                            // canCheckIn     0             1             0
-                            // canCheckOut    0             0             1
-                            // canTransfer    1             1             0
-                            if (item.isNeedCheckIn) {
-                                resultArr.push({
-                                    id: item.id,
-                                    startTime: item.startTime,
-                                    startTimeStr: item.startTimeStr,
-                                    canCancel: item.canCancel,
-                                    canCheckIn: item.canCheckIn,
-                                    canCheckOut: item.canCheckOut,
-                                    canTransfer: item.canTransfer
-                                });
-                            }
-                            return resultArr;
-                        }, []);
-                        // rooms which can check in right now
-                        let canCheckInRooms = roomList.filter(item => item.canCheckIn);
-                        // rooms need check in future
-                        let needCheckInRooms = roomList.filter(item => !item.canCheckIn && item.canCancel);
-                        console.log(`## <${canCheckInRooms.length}> rooms can check in right now. <${needCheckInRooms.length}> rooms need check in.`)
-                        let checkPrArr = canCheckInRooms.map(item => {
-                            return sendCheckInRequest(item.id);
-                        });
-                        Promise.all(checkPrArr).then(() => {
-                            resolve({
-                                cookieCheckStatus,
-                                requestStatus: true,
-                                status: SUCCESS,
-                                rooms: canCheckInRooms.length
-                            });
-                        }, () => reject);
-                    } else if (xhr.readyState === 4 && xhr.status !== 200) {
-                        throw Error(`Fail to get the room list, request error: ${xhr.status}`);
-                    }
-                } catch (err) {
-                    console.log(err);
-                    reject({
-                        cookieCheckStatus,
-                        requestStatus: false,
-                        status: FAIL_REQUEST,
-                        err
-                    });
-                }
-            };
-            xhr.onerror = function (e) {
-                console.log(e);
-                reject({
-                    cookieCheckStatus,
-                    requestStatus: false,
-                    status: FAIL_REQUEST,
-                    message: 'Fail to get the room list, request error'
-                });
-            };
-            xhr.ontimeout = function () {
-                reject({
-                    cookieCheckStatus,
-                    requestStatus: false,
-                    status: FAIL_TIMEOUT,
-                    message: 'Fail to get the room list, request timeout'
-                });
-            };
         }
-    });
-}
 
-function successResponse(res) {
-    // console.log('successRes')
-    // console.log(res)
-    let {
-        cookieCheckStatus,
-        requestStatus,
-        rooms
-    } = res;
-    if (cookieCheckStatus && requestStatus) {
-        chrome.browserAction.setIcon({
-            path: 'success.png'
+        if (res.redirected && /uuap.baidu.com\/login/g.test(res.url)) {
+            loginFailFromMe = true;
+            return await Promise.reject({
+                status: FAIL_LOGIN,
+                message: 'UUAP login fail!'
+            });
+        }
+        
+        loginFailFromMe = false;
+        const data = await res.json();
+        return data.data.list;
+    } catch (err) {
+        return await Promise.reject({
+            requestStatus: false,
+            status: err.status || FAIL_REQUEST,
+            message: err.message || `Fail to get the room list, request error!`,
         });
-        chrome.runtime.sendMessage({
-            action: 'popup-rooms-checked',
-            status: SUCCESS,
-            message: rooms > 0 ? `Checked ${rooms} rooms` : 'No rooms need to check.',
-            rooms
-        });
-        return {
-            status: 0,
-            rooms
-        };
     }
-}
+};
 
-function failResponse(err) {
-    console.log('!!-fail Response:-!!')
-    console.log(err);
-    let {
-        message,
-        status
-    } = err;
+async function fetchCheckIn(roomId) {
+    let url = `${meetingCheckInUrl}?scheduleId=${roomId}&random=${Math.random()}`;
+    try {
+        const res = await fetch(url, {
+            credentials: 'include'  
+        });
+
+        if (res.status !== 200) {
+            return await Promise.reject({
+                message: `Fail to checkin the room, request error: ${res.status}`
+            });
+        }
+        
+        return {
+            checkInStatus: SUCCESS
+        };
+    } catch (err) {
+        return await Promise.reject({
+            rrequestStatus: false,
+            status: FAIL_REQUEST,
+            message: err.message || `Fail to checkin the room, request error!`,
+            err
+        });
+    } 
+};
+
+
+function handleList(list) {
+    let roomList = list.reduce(function (resultArr, item) {
+        // status_field   havn't_start  can_check_in  hava_checked_in
+        // canCancel      1             1             0
+        // canCheckIn     0             1             0
+        // canCheckOut    0             0             1
+        // canTransfer    1             1             0
+        if (item.isNeedCheckIn) {
+            resultArr.push(item);
+        }
+        return resultArr;
+    }, []);
+    // rooms which can check in right now
+    let canCheckInRooms = roomList.filter(item => item.canCheckIn);
+    // rooms need check in future
+    let needCheckInRooms = roomList.filter(item => !item.canCheckIn && item.canCancel);
+    return {
+        needCheckInRooms,
+        canCheckInRooms
+    };
+};
+
+
+function successResponse(rooms) {
+    chrome.browserAction.setIcon({
+        path: 'success.png'
+    });
+    const checkLen = rooms.canCheckInRooms.length;
+    chrome.runtime.sendMessage({
+        action: 'popup-rooms-checked',
+        status: SUCCESS,
+        message: checkLen > 0 ? `Checked ${checkLen} rooms` : 'No rooms need to check.',
+        rooms
+    });
+
+    console.log(`${new Date().toLocaleString()}--runtime: ${++runtime}\n--checked ${checkLen} room(s)`);
+
+    if (checkLen > 0) {
+        chrome.notifications.create(null, {
+            type: 'basic',
+            iconUrl: 'success.png',
+            title: `${rooms.canCheckInRooms.map(({roomName}) => roomName)} already chenck-in!`,
+            message: 'Meeting Room Auto CheckIn!'
+        });
+    };
+};
+
+function failResponse({status, message}) {
     chrome.browserAction.setIcon({
         path: 'fail.png'
     });
 
     chrome.runtime.sendMessage({
         action: 'popup-rooms-checked',
-        status: status || 3,
+        status: status || 1,
         message: message || 'request failed!'
     });
-    return {
-        status,
-        message
-    };
-}
 
-// main check action
-let runtime = 0;
+    console.log(`${new Date().toLocaleString()}--runtime: ${++runtime}\n--${message}`);
 
-function checkMain() {
-    checkCookies().then(checkRooms).then(successResponse).catch(failResponse).then(res => {
-        if (res.status > 0) {
-            console.log(`${new Date().toLocaleString()}--runtime: ${++runtime}\n--${res.message}`);
-        } else {
-            console.log(`${new Date().toLocaleString()}--runtime: ${++runtime}\n--checked ${res.rooms} room(s)`);
-        }
-    })
-}
+    chrome.notifications.clear(failNotifyId, () => {
+        chrome.notifications.create(failNotifyId, {
+            type: 'basic',
+            iconUrl: 'fail.png',
+            title: `Meeting Auto CheckIn Fail!`,
+            message,
+            requireInteraction: true
+        });
+    });
+};
 
-function queryCookieStatus() {
-    checkCookies()
-        .then(
-            function (res) {
-                return {
-                    action: 'popup-cookies-checked',
-                    status: 0
-                };
-            },
-            function (res) {
-                return {
-                    action: 'popup-cookies-checked',
-                    status: 1
-                };
-            }
-        )
-        .then(res => {
-            // console.log('cookie checked');
-            chrome.browserAction.setIcon({
-                path: res.status > 0 ? 'fail.png' : 'success.png'
-            });
 
-            chrome.runtime.sendMessage(res);
-            if (res.status > 0) {
-                console.log(`${new Date().toLocaleString()}--query cookie status\n-- failed`);
-            } else {
-                console.log(`${new Date().toLocaleString()}--query cookie status\n-- success`);
-            }
-        })
-}
 
-let timer;
+async function checkMain() {
+    try {
+        const rooms = handleList(await fetchRoomList());
+        const {canCheckInRooms} = rooms;
+        const checkPrArr = canCheckInRooms.map(item => {
+            return fetchCheckIn(item.id);
+        });
+        await Promise.all(checkPrArr);
+        successResponse(rooms);
+    } catch (err) {
+        const {status, message} = err;
+        failResponse({
+            status,
+            message
+        });
+    }
+};
+
 
 function randomInvoke() {
     let base = 8 + Math.random() * 6;
@@ -253,23 +173,58 @@ function randomInvoke() {
         checkMain();
         base = 8 + Math.random() * 6;
         timer = setTimeout(invoke, base * 60 * 1000)
-    }, base * 60 * 1000)
+    }, base * 60 * 1000);
+    chrome.storage.sync.set({isStopTimer: false});
 }
-// start to work
-randomInvoke();
 
-// action from popup
-chrome.runtime.onMessage.addListener(function (request, sender) {
-    // instruction: check right now
-    if (request.action === 'background-check-now') {
-        if (timer) {
-            timer = null;
-        }
-        checkMain();
+
+chrome.storage.sync.get(['isStopTimer'], (res) => {
+    if (res.isStopTimer) {
+        chrome.browserAction.setIcon({
+            path: 'pause.png'
+        });
+    } 
+    else {
         randomInvoke();
-        timer = setInterval(checkMain, 10 * 60 * 1000);
-    // instruction: check cookies to judge login status
-    } else if (request.action === 'background-query-cookies-status') {
-        queryCookieStatus();
     }
 });
+
+
+// action from popup
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    switch (request.action) {
+        case 'background-check-now':
+            checkMain();
+            break;
+        case 'background-stop-now':
+            chrome.browserAction.setIcon({
+                path: 'pause.png'
+            });
+            clearTimeout(timer);
+            chrome.storage.sync.set({isStopTimer: true});
+            break;
+        case 'background-restart-now':
+            chrome.browserAction.setIcon({
+                path: 'success.png'
+            });
+            randomInvoke();
+            break;
+    }
+});
+
+chrome.notifications.onClicked.addListener(function (id) {
+    if (id === failNotifyId) {
+        chrome.notifications.clear(failNotifyId);
+        chrome.tabs.create({
+            url: 'http://meeting.baidu.com/#/home'
+        });
+    }
+});
+
+chrome.webRequest.onBeforeRedirect.addListener(function (details) {
+    const {initiator, redirectUrl} = details;
+    if (loginFailFromMe && /uuap.baidu.com/g.test(initiator) && /meeting.baidu.com/g.test(redirectUrl)) {
+        checkMain();
+    }
+}, {urls: ["*://meeting.baidu.com/*"]},
+["responseHeaders"]);
--- acmr/popup.js
+++ acmr/popup.js
@@ -1,64 +1,121 @@
 
-let checkBtn = document.getElementById('check-now-btn');
+const checkBtn = document.getElementById('check-now-btn');
+const loginBtn = document.getElementById('login-btn');
+const checkResult = document.getElementById('check-result');
+const roomList = document.getElementById('room-list');
+const controlBtn = document.getElementById('control')
+
+
+checkResult.success = function (text) {
+    this.style.color = '#67c23a';
+    this.style.backgroundColor = '#f0f9eb';
+    this.innerText = text;
+};
+
+checkResult.error = function (text) {
+    this.style.color = '#f56c6c';
+    this.style.backgroundColor = '#fef0f0';
+    this.innerText = text;
+};
+
+checkResult.clear = function () {
+    this.style.backgroundColor = '#fff';
+    this.innerText = '';
+};
+
+
+
+roomList.setList = function ({canCheckInRooms, needCheckInRooms}) {
+    let list1 = canCheckInRooms.length > 0 
+        ? `
+            <ul class="can-check">
+            ${canCheckInRooms.map(({roomName}) => 
+                `<li><span><img src="./ok.png">${roomName}</span></li>
+                `
+            ).join('')}
+            </ul>
+        ` 
+        : '';
+    let list2 = needCheckInRooms.length > 0
+        ? `
+            <ul class="need-check">
+            ${needCheckInRooms.map(({roomName, startTimeStr}) => 
+                `<li><span><img src="./wait.png">${roomName}</span><span>${startTimeStr}</span></li>
+                `
+            ).join('')}
+            </ul>
+        `
+        : '';
+    this.innerHTML = list1 + list2;
+};
+
 
 checkBtn.addEventListener('click', function() {
-    // send message to background to check rooms immediatelly
-    chrome.runtime.sendMessage({
-        action: 'background-check-now'
-    });
-})
-
-// const urlTestReg = /^http(?:s|):\/\/meeting\.baidu\.com.*home.*/;
-// let showNameBtn = document.getElementById('show-name-btn');
-// showNameBtn.addEventListener('click', function() {
-//     // send message to content scripts to change page style
-//     chrome.tabs.query({
-//         active: true,
-//         currentWindow: true
-//     }, function (tabs) {
-//         let tab = tabs[0];
-//         if (urlTestReg.test(tab.url)) {
-//             chrome.tabs.executeScript(null, {file: 'content.js'});
-//         }
-//     })
-// })
-
-window.onload = function() {
-    // check cookies when open popup
-    chrome.runtime.sendMessage({
-        action: 'background-query-cookies-status'
-    });
-}
-
-function setResultText({err, text, rooms}) {
-    // console.log('print result in pop');
-    // console.log(text);
-    let checkResult = document.getElementById('check-result');
-    if (err) {
-        checkResult.style.color = 'red';
-        checkResult.style.fontWeight = 'bold';
-    } else {
-        checkResult.style.fontWeight = 'normal';
-        checkResult.style.color = rooms && rooms > 0 ? '#96B97D' : 'black';
+    if (!this.getAttribute('disable')) {
+        chrome.runtime.sendMessage({
+            action: 'background-check-now'
+        });
     }
-    checkResult.innerText = text;
-}
+});
+
+controlBtn.addEventListener('click', function() {
+    const status = this.getAttribute('data-status');
+    switch (status) {
+        case 'run': {
+            this.setAttribute('data-status', 'stop');
+            checkBtn.setAttribute('disabled', true);
+            checkResult.error('auto service stopped.');
+            chrome.runtime.sendMessage({
+                action: 'background-stop-now'
+            });
+        }
+        break;
+        default: {
+            this.setAttribute('data-status', 'run');
+            checkBtn.removeAttribute('disabled');
+            checkResult.success('auto service restarted.')
+            chrome.runtime.sendMessage({
+                action: 'background-restart-now'
+            });
+        }
+    }
+});
 
 
 // background.js event listener
 chrome.runtime.onMessage.addListener(function(res) {
-    if (res.action === 'popup-cookies-checked') {
-        const opt = {
-            err: res.status > 0,
-            text: res.status > 0 ? 'cookie checked fail' : '',
-        };
-        setResultText(opt)
-    } else if (res.action === 'popup-rooms-checked') {
-        const opt = {
-            err: res.status > 0,
-            text: res.message,
-            rooms: res.rooms
-        };
-        setResultText(opt);
+    if (res.action === 'popup-rooms-checked') {
+        const {status, message, rooms} = res;
+        if (status === 0) {
+            checkResult.success(message);
+            roomList.setList(rooms);
+        }
+        else {
+            checkResult.error(message);
+        }
+        
+        status === 3 
+            ? (loginBtn.style.display = 'block') 
+            : (loginBtn.style.display = 'none');
     }
-})
+});
+
+
+// chrome.runtime.sendMessage({
+//     action: 'background-check-status'
+// }, ({status}) => {
+//     alert(status)
+//     if (status === 'pause') {
+//         controlBtn.setAttribute('data-status', 'stop');
+//         checkBtn.setAttribute('disabled', true);
+//         checkResult.error('auto service stopped!');
+//     }
+// });
+
+chrome.storage.sync.get(['isStopTimer'], (res) => {
+    if (res.isStopTimer) {
+        controlBtn.setAttribute('data-status', 'stop');
+        checkBtn.setAttribute('disabled', true);
+        checkResult.error('auto service stopped!');
+    }
+});
